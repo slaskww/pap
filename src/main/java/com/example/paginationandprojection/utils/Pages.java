ef9798c0d416d7adbde8b3180a916e3edd8db13b
lukasz <slaskww@gmail.com>
2019-10-11 10:36:36 +0200

    changed urls

--- src/main/java/com/example/paginationandprojection/utils/Pages.java
+++ src/main/java/com/example/paginationandprojection/utils/Pages.java
@@ -2,19 +2,19 @@ package com.example.paginationandprojection.utils;
 
 public interface Pages {
 
-    String HOME = "/home";
+    String HOME = "home";
 
     interface Registration{
 
-        String FORM = "/register-form";
-        String RESULT = "/register-result";
+        String FORM = "register-form";
+        String RESULT = "register-result";
     }
 
     interface Login{
-        String FORM = "/login-form";
+        String FORM = "login-form";
     }
 
     interface User{
-        String ACCOUNT = "/user-account";
+        String ACCOUNT = "user-account";
     }
 }
